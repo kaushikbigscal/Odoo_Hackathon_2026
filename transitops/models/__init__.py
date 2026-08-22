2a5b9616b14f5e90defa5c3c3c9c29580a5fcc9e
kaushikbigscal <154492517+kaushikbigscal@users.noreply.github.com>
2026-08-22 11:16:52 +0530

    Delete transitops directory

--- transitops/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import transitops_vehicle
-from . import transitops_driver
-from . import transitops_trip
-from . import transitops_maintenance
-from . import transitops_fuel_log
-from . import transitops_expense
-from . import dashboard
